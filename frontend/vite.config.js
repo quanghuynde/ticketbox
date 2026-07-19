e24df27a0af92df09f11a49c3aaa703b9e7ce041
quanghuynde <quanghuyn267@gmail.com>
2026-07-19 18:14:17 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: "/ticketbox/",
+
   server: {
-    headers: {},
     proxy: {
-      '/api': {
-        target: 'http://localhost:9999',
+      "/api": {
+        target: "http://localhost:9999",
         changeOrigin: true,
       },
     },
   },
-})
+});
